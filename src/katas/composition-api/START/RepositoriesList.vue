89ccd4d9e5bbf600a21d5608572f1c38d484e150
lmartinezs <martinez.s.luis@gmail.com>
2021-10-21 23:39:06 +0200

    Able to search github repos by user

---
+++ src/katas/composition-api/START/RepositoriesList.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul v-for="repo in repositories">
+    <li>
+      <a :href="repo.html_url">
+        {{repo.name}}
+      </a>
+    </li>
+  </ul>
+</template>repositories
+
+<script>
+export default {
+  name: "RepositoriesList",
+  props: {
+    repositories: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
--- src/katas/composition-api/START/UserRepositories.vue
+++ src/katas/composition-api/START/UserRepositories.vue
@@ -1,48 +1,70 @@
 <template>
   <div>
-    REPOSITORIES
+    <label for="githubUser">
+      Github user
+      <input id="githubUser" type="text" v-model="user" />
+    </label>
+    <button @click="handleClick">search repos</button>
+    <RepositoriesList :repositories="repositories" />
   </div>
 </template>
 
 <script>
+import RepositoriesList from './RepositoriesList.vue'
+
 export default {
-  // components: { RepositoriesFilters, RepositoriesSortBy, RepositoriesList },
-  props: {
-    user: {
-      type: String,
-      required: true
-    }
+  components: {
+    // RepositoriesFilters,
+    // RepositoriesSortBy,
+    RepositoriesList
   },
-  data () {
+  // props: {
+  //   user: {
+  //     type: String,
+  //     required: true
+  //   }
+  // },
+  data() {
     return {
       repositories: [], // 1
       filters: {
         // filters
-       }, // 3
-      searchQuery: '' // 2
+      }, // 3
+      searchQuery: '', // 2
+      user: 'luismartinezs'
     }
   },
   computed: {
-    filteredRepositories () {
+    filteredRepositories() {
       // filter
-     }, // 3
-    repositoriesMatchingSearchQuery () {
+    }, // 3
+    repositoriesMatchingSearchQuery() {
       // search query
-     }, // 2
+    }, // 2
   },
-  watch: {
-    user: 'getUserRepositories' // 1
-  },
+  // watch: {
+  //   user: 'getUserRepositories' // 1
+  // },
   methods: {
-    getUserRepositories () {
+    async getUserRepositories() {
       // using `this.user` to fetch user repositories
+      try {
+        const res = await fetch(`https://api.github.com/users/${this.user}/repos?per_page=100`)
+        return res.json()
+      } catch (err) {
+        console.error(err)
+        return []
+      }
     }, // 1
-    updateFilters () {
+    updateFilters() {
       // do the update
-     }, // 3
+    }, // 3
+    async handleClick () {
+      this.repositories = await this.getUserRepositories()
+    }
   },
-  mounted () {
-    this.getUserRepositories() // 1
+  async mounted() {
+    this.repositories = await this.getUserRepositories() // 1
   }
 }
 </script>
